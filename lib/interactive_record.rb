1cfd00dee5da681e7d044476c67c306abc89c005
Cloudymolecule <baires82@gmail.com>
2020-10-19 17:24:37 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,73 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
-  
+
+    def initialize(attributes={})
+        attributes.each do |k, v|
+            self.send("#{k}=", v)
+        end
+    end
+
+    def self.table_name
+        self.to_s.downcase.pluralize
+    end
+    
+    def self.column_names
+        sql = "PRAGMA table_info('#{table_name}')"
+
+        table_info = DB[:conn].execute(sql)
+        column_names = []
+        
+        table_info.each do |column|
+            column_names << column["name"]
+        end
+        column_names.compact
+        
+        column_names.each do |col_name|
+            attr_accessor col_name.to_sym
+        end
+    end
+
+    def table_name_for_insert
+        self.class.table_name
+    end
+
+    def col_names_for_insert
+        self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+    end
+
+    def values_for_insert
+        values = []
+        vals = self.class.column_names
+        vals.each do |c_n|
+            values << "'#{send(c_n)}'" unless send(c_n).nil?
+        end
+        values.join(", ")
+    end
+
+    def save
+        DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})")
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    end
+
+    def self.find_by_name(name)
+        sql = <<-SQL
+        SELECT * FROM #{self.table_name} WHERE name = ?
+        SQL
+
+        DB[:conn].execute(sql, name)
+    end
+
+    def self.find_by(attribute)
+        key = attribute.flatten[0].to_s
+        val = attribute.flatten[1]
+        
+        sql = <<-SQL
+        SELECT * FROM #{self.table_name} WHERE #{key} = ?
+        SQL
+        
+        DB[:conn].execute(sql, val)
+    end
 end
